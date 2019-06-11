41cbb5f7bc47ecfcda99de3b46364dde54eeb15e
oGsLP <1145234011@qq.com>
2019-06-11 17:15:36 +0800

    :arrow_up: vuepress=>1x

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,31 @@
 <template>
-  <div>
-    <l-button
-      icon="message-square"
-      :gap="true"
-      iconPosition="left"
-      poke="grass"
-      size="immense"
-      >宝贝球</l-button
-    >
-    <l-button-group style="margin: 20px">
-      <l-button icon="arrow-left-circle" poke="latios">sa</l-button>
-      <l-button>sad</l-button>
-      <l-button icon="arrow-right-circle" iconPosition="right">sa</l-button>
-    </l-button-group>
+  <div class="app">
+    <!--<l-icon-->
+    <!--class="test"-->
+    <!--icon="mic"-->
+    <!--color="blue"-->
+    <!--style="font-size: 56px"-->
+    <!--&gt;</l-icon>-->
+    <!--<l-button-->
+    <!--class="test"-->
+    <!--icon="pokemon-ball"-->
+    <!--:gap="true"-->
+    <!--iconPosition="right"-->
+    <!--poke="fairy"-->
+    <!--size="immense"-->
+    <!--&gt;宝贝球</l-button-->
+    <!--&gt;-->
+    <!--<l-button-group class="test">-->
+    <!--<l-button icon="arrow-left-circle" poke="latios">pre</l-button>-->
+    <!--<l-button>...</l-button>-->
+    <!--<l-button icon="arrow-right-circle" iconPosition="right">next</l-button>-->
+    <!--</l-button-group>-->
+
+    <l-dropdown poke="fire" class="test">
+      dasdasdas
+    </l-dropdown>
+
+    <l-icon-button icon="list" poke="rock" class="test"></l-icon-button>
   </div>
 </template>
 
@@ -22,4 +35,11 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.app {
+  > .test {
+    margin: 20px;
+    vertical-align: middle;
+  }
+}
+</style>
